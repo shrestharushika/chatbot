d41e16085390911f2b119a9c19fa814c9b129202
shrestharushika <46518960+shrestharushika@users.noreply.github.com>
2021-07-11 17:02:11 +0530

    Update chatbottrain.py

--- chatbottrain.py
+++ chatbottrain.py
@@ -10,8 +10,8 @@ def set():
     logging.basicConfig(filename='example.log',level=logging.DEBUG)
    
     trainer=ListTrainer(chatbot)
-    for files in os.listdir('C:/Users/Dell/AppData/Local/Programs/Python/Python38-32/Scripts/env/Scripts/chatbot/data/'):
-        data=open('C:/Users/Dell/AppData/Local/Programs/Python/Python38-32/Scripts/env/Scripts/chatbot/data/' + files,encoding='latin-1').readlines()   
+    for files in os.listdir('C:/Users/Dell/Desktop/chatbot/data/'):
+        data=open('C:/Users/Dell/Desktop/chatbot/data/chatbot/data/' + files,encoding='latin-1').readlines()   
         trainer.train(data) 
 
                  
